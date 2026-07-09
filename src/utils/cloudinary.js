d57f5fd448a5390a83536237ca33ebf9361643aa
Sheikh Muhammad Fahad <Sheikh.MuhammadFahad@abacusone.local>
2026-07-09 15:52:54 +0500

    created user route&controllar confi(multer&cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+
+
+
+   cloudinary.config({ 
+        cloud_name: process.env., 
+        api_key: '', 
+        api_secret: '' // Click 'View API Keys' above to copy your API secret
+    });
