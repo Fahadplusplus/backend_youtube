af736491e3b61c69bb4f832e475b0b956d30d724
Sheikh Muhammad Fahad <Sheikh.MuhammadFahad@abacusone.local>
2026-07-15 10:24:43 +0500

    get access/refresh token

--- src/controllers/user.controllar.js
+++ src/controllers/user.controllar.js
@@ -3,6 +3,31 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {User} from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { apiResponse } from "../utils/apiResponse.js";
+
+const generateAccessAndRefreshTokens = async(userId) => {
+   try
+       {
+    
+     const user = await User.findById(userId)
+    console.log(user);
+    
+     const accessToken = user.generateAccessToken()
+     const refreshToken = user.generateRefreashToken()
+     console.log(accessToken,"asscess token");
+
+     user.refreshToken = refreshToken
+     await user.save({ validateBeforeSave: false })
+     return{accessToken,refreshToken}
+      
+   } catch (error) {
+         console.log("TOKEN ERROR:", error);
+   throw error;
+      throw new apiError(500, "Somthing went wrong while generating assess and refresh token")
+   }
+}
+
+
+
 const registerUser = asyncHandler( async(req,res)=>{
    //Receive data from users
    //validate that data 
@@ -11,46 +36,23 @@ const registerUser = asyncHandler( async(req,res)=>{
    //create user object - create entery in DB
    //remove password and refres token field form response
    //check if user is created
- 
-   
    const {fullName,email,username,password}= req.body
-  
-
    if(
       [fullName,email,username,password].some((field) => field?.trim() === "" )
    ){
       throw new apiError(400,"All fields are required")
    }
-
-   
-   
    const existingUser=await User.findOne({
       $or: [{email},{username}]
    })
    if(existingUser){
      throw new apiError(409,"user with username or email already exists")
    }
-   
-   // const existingEmail= await User.findOne(email)
-   // if(existingEmail){
-   //   throw new apiError(409,"email already exists")
-   // }
-   //   const existingUsername= await User.findOne(username)
-   // if(existingUsername){
-   //   throw new apiError(409,"Username already exists")
-   // }
-
-
-
   const profilePicLocalPath = req.files?.profilePic[0]?.path;
 const coverImgLocalPath = req.files?.coverImage?.[0]?.path;
-
-
-
    if(!profilePicLocalPath){
       throw new apiError(400, "profile Image not found")
    }
-
    const profilePic = await uploadOnCloudinary(profilePicLocalPath)
    const coverImage = await uploadOnCloudinary(coverImgLocalPath)
 
@@ -73,22 +75,57 @@ const coverImgLocalPath = req.files?.coverImage?.[0]?.path;
 
    if (!createduser){
       throw new apiError(500,"Something went wrong while registring the user")
-   }
-  
+   }  
    return res.status(201).json(
-      new apiResponse(200, createduser, "User registered successfully"
-
-      )
-   )
-
-
-  
-
-   
+      new apiResponse(200, createduser, "User registered successfully")
+    )
 })
 
 const loginUser = asyncHandler( async( req , res ) =>{
+   
+   
+      const {email,username,password} = req.body;
+   
+      if(!username && !email){
+         throw new apiError(400,"username or email is required")
+      }
 
+      const user =await User.findOne({
+         $or:[{username},{email}]
+      })
+      if(!user){
+         throw new apiError(404, "user does not exists")
+      }
+      
+      const isPassowrdValid = await user.isPasswordCorrect(password)
+        if(!isPassowrdValid){
+         throw new apiError(401, "Password Incorrect")
+      }
+
+     const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+     const options ={
+      httpOnly: true,
+      secure: true
+     }
+
+     return res
+     .status(200)
+     .cookie("accessToken", accessToken, options)
+     .cookie("refreshToken", refreshToken, options)
+     .json(
+      new apiResponse(
+         200,
+         {
+            user: loggedInUser,accessToken,refreshToken
+         },
+         "User logged In Successfully"
+      )
+     )
+
+       
 })
 
 export {registerUser,loginUser}
